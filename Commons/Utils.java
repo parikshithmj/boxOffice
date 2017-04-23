80e10e5602adb265478549a5fb538ca58768c202
Parikshith Malalur Jagadeesh <parikshithmj@users.noreply.github.com>
2017-04-22 18:36:02 -0700

    Adding a const

--- Commons/Utils.java
+++ Commons/Utils.java
@@ -11,6 +11,8 @@ import java.io.IOException;
  */
 public class Utils {
     public final static String MOVIEDB_API_KEY = "d07387463c2c0d2998d6bd09ec481c17";
+    public final static String FB_ACCESS_TOKEN = "EAACEdEose0cBAKZCKElT1C34t9n4Hw3hdNGdZAZCzkZCfA3BkC2YJL0Fy4VsqbGDkd9DVhSQ5ST7KrDna5SCLEjZBLcbeZByhZAUUq5jLqW7MzLcROWjUZAtoD8bEuaBC06YnJ1mIZAiwTB0R1mL7ILrirulY2gXLRJmrliS6aZA7gV2FomhJogS09OOiosg0NiHYZD";
+
     public static String httpRequest(Request request){
         String json= "";
         OkHttpClient client = new OkHttpClient();
