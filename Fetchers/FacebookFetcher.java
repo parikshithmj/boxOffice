81ab20bf1fcd94a15f57262aaf2c4caf5e2aa403
parmj <parikshithmj@gmail.com>
2017-04-22 18:29:37 -0700

    Facebook4j doesn't support reactions as it implements older version of Graph API.Hence adding HTTP client to get reactions.

--- Fetchers/FacebookFetcher.java
+++ Fetchers/FacebookFetcher.java
@@ -1,32 +1,72 @@
 package Fetchers;
 
+
+import Commons.Utils;
 import facebook4j.*;
 import facebook4j.auth.AccessToken;
+import javafx.geometry.Pos;
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by parmj on 2/15/17.
  */
 public class FacebookFetcher {
-    public void getReactions(String id){
-        Facebook facebook = new FacebookFactory().getInstance();
-        String appId = "1091248824328225", appSecret = "23f8176e7c61b61457fb51a04a2b58ef";
 
-        String commaSeparetedPermissions = "email,publish_stream";
-        String accessToken = "EAAPgfCqJICEBACOoteUnMeKDZCmQNszAumilHDt2ylKlfW2AHbfRZCXN8iDCZAd4MsKNMSBwzS0M5rowKmC6who7GxZCTZAuR7h6URSEdlEwmQ62tdeRWdiDS76mZA8ZCF61WS3OCfOL36l8erhEqhB1ZBlKJZAYoya4QAmV5jxHF69XQ2Cw9xDgkcTCuDoRS5JUZD";
-        facebook.setOAuthAppId(appId, appSecret);
-        facebook.setOAuthPermissions(commaSeparetedPermissions);
-        facebook.setOAuthAccessToken(new AccessToken(accessToken, null));
+    //Input : Facebook Post Id
+    public void getReactions(String id) {
 
-        try {
-            ResponseList<Reaction> reactions = facebook.getPostReactions(id);
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{}");
+        Request request = null;
+        Map<String, Integer> reactionsMap= new HashMap<String,Integer>();
+        String data = null;
 
-            for(Reaction r: reactions){
-                System.out.println("REACTION............"+r.getType());
+        String url = "https://graph.facebook.com/v2.9/"+id+"/reactions?access_token="+ Utils.FB_ACCESS_TOKEN+"&fields=type";
+        do{
+             request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+            try{
+                OkHttpClient client = new OkHttpClient();
+                Response response = client.newCall(request).execute();
+                String json = response.body().string();
+                System.out.println(json);
+                JSONObject jsonObject = new JSONObject(json);
+                JSONArray resultsData = (JSONArray)jsonObject.get("data");
+                JSONObject resultsPaging = jsonObject.getJSONObject("paging");
+                 for(int i = 0; i<resultsData.length();i++){
+                    String type = (String)resultsData.getJSONObject(i).get("type");
+                    if(reactionsMap.containsKey(type)){
+                        int tmp = reactionsMap.get(type);
+                        tmp++;
+                        reactionsMap.put(type,tmp);
+                    }
+                    else{
+                        reactionsMap.put(type,1);
+                    }
+
+                }
+                if(resultsPaging.has("next"))
+                    url = (String)resultsPaging.get("next");
+                else
+                    url = null;
+                            }catch(Exception e){
+                e.printStackTrace();
             }
-        } catch (FacebookException e) {
-            e.printStackTrace();
-        }
+
+        }while(url!=null);
+
+//            MongoCRUD mongoCRUD = new MongoCRUD();
+//            mongoCRUD.insert(json, "boxoffice", "upcomingMovies");
 
     }
 
 }
+
+
